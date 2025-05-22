9c11ae8496c7de0d6e106543986ee7dc2743af3d
Kumar Santosh <phpdevks@gmail.com>
2025-05-22 14:23:07 +0530

    commit

--- src/Components/HomeClients.js
+++ src/Components/HomeClients.js
@@ -5,59 +5,55 @@ return(
 
 <div id="ourclients">
     <section>
-      <div class="row">
-        
-	<div class="section-title text-center">                        
-		<h3>Our Clients </h3>       
-
-            </div>    
-      </div>
-      <div class="row">
-        <div class="column">
-          <div class="card">
-             <div class="icon-services">
-             <img src="client_images/client-1.png" className="client_icon" />
+      <div className="row">
+        <div className="section-title text-center" style={{ display:"block",clear:"both", width:"100%" }}>                        
+		<h3>Our Clients </h3>  
+</div>        
+        <div className="column">
+          <div className="card">
+             <div className="icon-services">
+             <img src="client_images/client-1.png" classNameName="client_icon" />
             </div>           
                      
           </div>
         </div>
 
-        <div class="column">
-          <div class="card">           
-               <div class="icon-services">
-             <img src="client_images/client-2.png" className="client_icon" />
+        <div className="column">
+          <div className="card">           
+               <div className="icon-services">
+             <img src="client_images/client-2.png" classNameName="client_icon" />
             </div>           
           </div>
         </div>
 
-        <div class="column">
-          <div class="card">           
-               <div class="icon-services">
-             <img src="client_images/client-3.png" className="client_icon" />
+        <div className="column">
+          <div className="card">           
+               <div className="icon-services">
+             <img src="client_images/client-3.png" classNameName="client_icon" />
             </div>           
           </div>
         </div>
 
-        <div class="column">
-          <div class="card">           
-              <div class="icon-services">
-             <img src="client_images/client-4.png" className="client_icon" />
+        <div className="column">
+          <div className="card">           
+              <div className="icon-services">
+             <img src="client_images/client-4.png" classNameName="client_icon" />
             </div>            
           </div>
         </div>
 
-        <div class="column">
-          <div class="card">           
-               <div class="icon-services">
-             <img src="client_images/client-5.png" className="client_icon" />
+        <div className="column">
+          <div className="card">           
+               <div className="icon-services">
+             <img src="client_images/client-5.png" classNameName="client_icon" />
             </div>          
           </div>
         </div>
 
-        <div class="column">
-          <div class="card">           
-               <div class="icon-services">
-             <img src="client_images/client-6.png" className="client_icon" />
+        <div className="column">
+          <div className="card">           
+               <div className="icon-services">
+             <img src="client_images/client-6.png" classNameName="client_icon" />
             </div>          
           </div>
         </div>
